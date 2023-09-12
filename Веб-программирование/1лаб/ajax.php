603989e98f176c020bc3a072faa20cecd8a85de5
Kostya <kostikbushmelev@gmail.com>
2023-09-13 00:52:55 +0300

    remove useless code

--- Веб-программирование/1лаб/ajax.php
+++ Веб-программирование/1лаб/ajax.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Передача переменных из PHP в JavaScript</title>
-</head>
-<body>
-
 <?php
 require_once("response/Coordinator.php");
 require_once("response/Validator.php");
@@ -12,13 +5,18 @@ require_once("database/Connector.php");
 require_once("database/Migrations.php");
 require_once("database/Query.php");
 
-$x = $_GET["x"];
-$y = $_GET["y"];
-$R = $_GET["R"];
+//$x = $_GET["x"];
+//$y = $_GET["y"];
+//$R = $_GET["R"];
+
+$x = (float)$_GET["text"];
+$y = (float)$_GET["radio"];
+$R = (float)$_GET["press_button"];
 
 ?>
 
 <script>
+    console.log("зашел в ajax");
     var x = <?php echo $x; ?>;
     var y = <?php echo $y; ?>;
     var R = <?php echo $R; ?>;
@@ -31,203 +29,200 @@ $runner->run();
 
 class Runner
 {
-    private float $x;
-    private float $y;
-    private float $R;
+private float $x;
+private float $y;
+private float $R;
 
-    /**
-     * @param float $x
-     * @param float $y
-     * @param float $R
-     */
-    public function __construct(float $x, float $y, float $R)
-    {
-        $this->x = $x;
-        $this->y = $y;
-        $this->R = $R;
+/**
+ * @param float $x
+ * @param float $y
+ * @param float $R
+ */
+public function __construct(float $x, float $y, float $R)
+{
+    $this->x = $x;
+    $this->y = $y;
+    $this->R = $R;
+}
+
+public function run()
+{
+$startTime = microtime(true);
+
+$validator = new Validator();
+$string = $validator->check($this->x, $this->y, $this->R);
+echo $string;
+
+$objCoordinator = new Coordinator($this->x, $this->y, $this->R);
+$res = $objCoordinator->getAnswer();
+
+$workTime = microtime(true) - $startTime;
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Передача переменных из PHP в JavaScript</title>
+</head>
+<body>
+<table border="1" class="data-table" id="data-table">
+    <caption class="gradient-blue">Таблица результатов запроса</caption>
+    <tr>
+        <th class="data-header">id</th>
+        <th class="data-header">x</th>
+        <th class="data-header">y</th>
+        <th class="data-header">R</th>
+        <th class="data-header">Попадание</th>
+        <th class="data-header">Время запроса</th>
+        <th class="data-header">Время обработки запроса(мкс)</th>
+    </tr>
+    <tbody id="data-body">
+
+    </tbody>
+</table>
+
+<script>
+    class Query {
+        constructor(id, x, y, R, result, workTime) {
+            this._id = id;
+            this._x = x;
+            this._y = y;
+            this._R = R;
+            this._result = result;
+            this._workTime = workTime;
+        }
+
+        get id() {
+            return this._id;
+        }
+
+        set id(value) {
+            this._id = value;
+        }
+
+        get x() {
+            return this._x;
+        }
+
+        set x(value) {
+            this._x = value;
+        }
+
+        get y() {
+            return this._y;
+        }
+
+        set y(value) {
+            this._y = value;
+        }
+
+        get R() {
+            return this._R;
+        }
+
+        set R(value) {
+            this._R = value;
+        }
+
+        get result() {
+            return this._result;
+        }
+
+        set result(value) {
+            this._result = value;
+        }
+
+        get workTime() {
+            return this._workTime;
+        }
+
+        set workTime(value) {
+            this._workTime = value;
+        }
     }
 
-    public function run()
-    {
-        $startTime = microtime(true);
+    class TablePrinter {
+        constructor() {
+        }
 
-        $validator = new Validator();
-        $string = $validator->check($this->x, $this->y, $this->R);
-        echo $string;
+        run() {
+            var array;
+            try {
+                var stringArray = localStorage.getItem("array");
+                array = JSON.parse(stringArray);
+            } catch
+                (error) {
+                console.log(error);
+            } finally {
+                array = array || [];
+                let res = <?php echo $res; ?>;
+                let workTime = <?php echo $workTime; ?>;
+                let id = array.length || 0;
+                id += 1;
 
-        $objCoordinator = new Coordinator($this->x, $this->y, $this->R);
-        $res = $objCoordinator->getAnswer();
+                let query = new Query(id, x, y, R, res, workTime);
 
-        $workTime = microtime(true) - $startTime;
-
-        ?>
-
-        <table border="1" class="data-table" id="data-table">
-            <caption class="gradient-blue">Таблица результатов запроса</caption>
-            <tr>
-                <th class="data-header">id</th>
-                <th class="data-header">x</th>
-                <th class="data-header">y</th>
-                <th class="data-header">R</th>
-                <th class="data-header">Попадание</th>
-                <th class="data-header">Время запроса</th>
-                <th class="data-header">Время обработки запроса(мкс)</th>
-            </tr>
-            <tbody id="data-body">
-
-            </tbody>
-            <!--            <tr>-->
-            <!--                <td class="id-color" id ="x">get</td>-->
-            <!--                <td id="y"></td>-->
-            <!--                <td id="R"></td>-->
-            <!--                <td class="red-response" id = "red-res"></td>-->
-            <!--                <td class="green-response" id="green-res"></td>-->
-            <!--                <td id="start_time"></td>-->
-            <!--                <td id="end_time"></td>-->
-            <!--            </tr>-->
-        </table>
-
-        <script>
-            class Query {
-                constructor(id, x, y, R, result, workTime) {
-                    this._id = id;
-                    this._x = x;
-                    this._y = y;
-                    this._R = R;
-                    this._result = result;
-                    this._workTime = workTime;
-                }
-
-                get id() {
-                    return this._id;
-                }
-
-                set id(value) {
-                    this._id = value;
-                }
-
-                get x() {
-                    return this._x;
-                }
-
-                set x(value) {
-                    this._x = value;
-                }
-
-                get y() {
-                    return this._y;
-                }
-
-                set y(value) {
-                    this._y = value;
-                }
-
-                get R() {
-                    return this._R;
-                }
-
-                set R(value) {
-                    this._R = value;
-                }
-
-                get result() {
-                    return this._result;
-                }
-
-                set result(value) {
-                    this._result = value;
-                }
-
-                get workTime() {
-                    return this._workTime;
-                }
-
-                set workTime(value) {
-                    this._workTime = value;
-                }
+                array.push(query);
+                stringArray = JSON.stringify(array);
+                localStorage.setItem("array", stringArray);
             }
 
-            class TablePrinter {
-                constructor() {
+            let stringObject = localStorage.getItem("array");
+            var object = JSON.parse(stringObject);
+
+            let size = object.length;
+
+            var tbody = document.getElementById("data-table");
+
+            for (let i = 0; i < size; i++) {
+                var row = tbody.insertRow(-1);
+
+                var cell = row.insertCell(0);
+                let data = object[i]._id;
+                let newData = document.createTextNode(data);
+                cell.classList.add('id-color');
+                cell.appendChild(newData);
+                cell = row.insertCell(1);
+                data = object[i]._x;
+                newData = document.createTextNode(data);
+                cell.appendChild(newData);
+                cell = row.insertCell(2);
+                data = object[i]._y;
+                newData = document.createTextNode(data);
+                cell.appendChild(newData);
+                cell = row.insertCell(3);
+                data = object[i]._R;
+                newData = document.createTextNode(data);
+                cell.appendChild(newData);
+                cell = row.insertCell(4);
+                data = object[i]._result;
+                if (data) {
+                    data = "Да";
+                    cell.classList.add('green-response');
+                } else {
+                    data = "Нет";
+                    cell.classList.add('red-response');
                 }
-
-                run() {
-                    var array;
-                    try {
-                        var stringArray = localStorage.getItem("array");
-                        array = JSON.parse(stringArray);
-                    } catch
-                        (error) {
-                    } finally {
-                        array = array || [];
-                        let res = <?php echo $res; ?>;
-                        let workTime = <?php echo $workTime; ?>;
-                        let id = array.length || 0;
-                        id += 1;
-
-                        let query = new Query(id, x, y, R, res, workTime);
-
-                        array.push(query);
-                        stringArray = JSON.stringify(array);
-                        localStorage.setItem("array", stringArray);
-                    }
-
-                    let stringObject = localStorage.getItem("array");
-                    var object = JSON.parse(stringObject);
-
-                    let size = object.length;
-
-                    var tbody = document.getElementById("data-table");
-
-                    for (let i = 0; i < size; i++) {
-                        var row = tbody.insertRow(-1);
-
-                        var cell = row.insertCell(0);
-                        let data = object[i]._id;
-                        let newData = document.createTextNode(data);
-                        cell.classList.add('id-color');
-                        cell.appendChild(newData);
-                        cell = row.insertCell(1);
-                        data = object[i]._x;
-                        newData = document.createTextNode(data);
-                        cell.appendChild(newData);
-                        cell = row.insertCell(2);
-                        data = object[i]._y;
-                        newData = document.createTextNode(data);
-                        cell.appendChild(newData);
-                        cell = row.insertCell(3);
-                        data = object[i]._R;
-                        newData = document.createTextNode(data);
-                        cell.appendChild(newData);
-                        cell = row.insertCell(4);
-                        data = object[i]._result;
-                        if (data) {
-                            data = "Да";
-                            cell.classList.add('green-response');
-                        } else {
-                            data = "Нет";
-                            cell.classList.add('red-response');
-                        }
-                        newData = document.createTextNode(data);
-                        cell.appendChild(newData);
-                        cell = row.insertCell(5);
-                        data = new Date().toLocaleString();
-                        newData = document.createTextNode(data);
-                        cell.appendChild(newData);
-                        cell = row.insertCell(6);
-                        data = object[i]._workTime.toFixed(6);
-                        newData = document.createTextNode(data);
-                        cell.appendChild(newData);
-                    }
-                    console.log(object);
-                }
+                newData = document.createTextNode(data);
+                cell.appendChild(newData);
+                cell = row.insertCell(5);
+                data = new Date().toLocaleString();
+                newData = document.createTextNode(data);
+                cell.appendChild(newData);
+                cell = row.insertCell(6);
+                data = object[i]._workTime.toFixed(6);
+                newData = document.createTextNode(data);
+                cell.appendChild(newData);
             }
-
-            let tablePrinter = new TablePrinter();
-            tablePrinter.run();
-        </script>
-        <?php
+            console.log(object);
+        }
     }
+
+    let tablePrinter = new TablePrinter();
+    tablePrinter.run();
+</script>
+<?php
+}
 }
 
 ?>
--- Веб-программирование/1лаб/script/send-ajax-request.js
+++ Веб-программирование/1лаб/script/send-ajax-request.js
@@ -1,24 +1,15 @@
-$('form').on('submit', function (e) {
-    e.preventDefault();
+$("#submit-button").click(function (e) {
+    // e.preventDefault();
+    var form = $("form");
+
     $.ajax({
-        url: $(this).attr('action'),
         type: "GET",
-        cache: false,
-        data: $(this).serialize(),
-        success: function (data) {
-            var response = JSON.parse(data);
-            var x = response.radio;
-            var y = response.text;
-            var R = response.press_button;
-            $.ajax({
-                type: "GET",
-                url: "ajax.php",
-                data: {x: x, y: y, R: R},
-                success: function (response) {
-                    $('.data-table').html(response);
-                    x
-                }
-            })
+        url: "ajax.php",
+        data: form.serialize(),
+        success: function (response) {
+            alert(response);
+            $('.data-table').html(response);
+            form[0].reset();
         }
     });
 })
